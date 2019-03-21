6d28d5c909ecfb7caecc781780fa2f852915a3b6
Przemek Lewandowski <prze.lewandowski@gmail.com>
2019-03-21 11:22:27 +0100

    Add projects alias.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,6 @@
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias dotfiles="cd $DOTFILES"
+alias projects="cd ~/dev/projects"
 alias docker-composer="docker-compose"
 
