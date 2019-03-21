ce99cf3d5e055598ba8d62fb4bf27f93a3391e15
Przemek Lewandowski <prze.lewandowski@gmail.com>
2019-03-21 11:19:47 +0100

    Add virtualenvwrapper setup

--- zsh/powerlevel9k.zsh
+++ zsh/powerlevel9k.zsh
@@ -12,7 +12,7 @@ POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%F{blue}\u256D\u2500%F{white}"
 POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{blue}\u2570\uf460%F{white} "
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context root_indicator dir dir_writable_joined)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time
-                                    vcs background_jobs_joined time_joined)
+                                    virtualenv vcs background_jobs_joined time_joined)
 POWERLEVEL9K_VCS_STAGED_ICON="\u00b1"
 POWERLEVEL9K_VCS_UNTRACKED_ICON="\u25CF"
 POWERLEVEL9K_VCS_UNSTAGED_ICON="\u00b1"
---
+++ zsh/virtualenvwrapper.zsh
@@ -0,0 +1,5 @@
+export WORKON_HOME=~/dev/virtualenvs
+export PROJECT_HOME=~/dev/projects
+export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python3"
+source /usr/local/bin/virtualenvwrapper.sh
+
