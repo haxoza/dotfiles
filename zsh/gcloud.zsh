98ca622ddbb696268f68aa8df1f42294f04255f5
Przemek Lewandowski <prze.lewandowski@gmail.com>
2021-03-04 15:17:48 +0100

    add config for gcloud from brew

---
+++ zsh/gcloud.zsh
@@ -0,0 +1,3 @@
+export CLOUDSDK_PYTHON="/usr/local/opt/python@3.8/libexec/bin/python"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
