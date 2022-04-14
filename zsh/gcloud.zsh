ab627f061b6fef32771cb0503b8f33970f242af2
Przemek Lewandowski <prze.lewandowski@gmail.com>
2022-04-14 15:16:54 +0200

    Update brew packages and configs for m1 device

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,3 +1,3 @@
-export CLOUDSDK_PYTHON="/usr/local/opt/python@3.8/libexec/bin/python"
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+export CLOUDSDK_PYTHON="/opt/homebrew/opt/python@3.8/libexec/bin/python"
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
