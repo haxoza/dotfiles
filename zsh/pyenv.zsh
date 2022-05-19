181ba4647c93c66e90fd537363941ea939542b0d
Przemek Lewandowski <prze.lewandowski@gmail.com>
2022-05-19 17:28:48 +0200

    add pyenv instead of virtualenvwrapper

---
+++ zsh/pyenv.zsh
@@ -0,0 +1,3 @@
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
--- zsh/virtualenvwrapper.zsh
+++
@@ -1,5 +0,0 @@
-export WORKON_HOME=~/dev/virtualenvs
-export PROJECT_HOME=~/dev/projects
-export VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python3"
-source /usr/local/bin/virtualenvwrapper.sh
-
