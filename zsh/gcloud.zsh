448b2e030927c4984f2913483237d844caa827ed
Przemek Lewandowski <prze.lewandowski@gmail.com>
2024-03-13 20:13:03 +0100

    Amend gcloud cli not to use python 3.8

--- zsh/gcloud.zsh
+++ zsh/gcloud.zsh
@@ -1,3 +1,2 @@
-export CLOUDSDK_PYTHON="/opt/homebrew/opt/python@3.8/libexec/bin/python"
 source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
