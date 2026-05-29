710758608f64fea71f5bd6f3a934391b280e5aa1
Przemek Lewandowski <prze.lewandowski@gmail.com>
2026-05-29 11:51:52 +0200

    fix(zsh): disable git pager in dev update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,16 +19,14 @@ station-off() {
 
 # Update dev tools: Homebrew packages and Zinit plugins
 dev-update() {
-  echo "==> Updating Homebrew..."
   brew update && brew upgrade
 
   echo "==> Updating Zinit and plugins..."
-  zinit self-update
-  zinit update --all
+  GIT_PAGER='' zinit self-update
+  GIT_PAGER='' zinit update --all
 
   echo "==> Clearing Zinit completions cache..."
   zi cclear
 
   echo "==> Done!"
 }
-
